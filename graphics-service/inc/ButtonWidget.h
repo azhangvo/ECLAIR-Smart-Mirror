67be72841a4f5dadd8c8682fa3230506c210903c
Arthur Zhang <azhangvo@gmail.com>
2022-09-15 18:52:57 -0500

    button framing

---
+++ graphics-service/inc/ButtonWidget.h
@@ -0,0 +1,14 @@
+//
+// Created by endian on 9/12/22.
+//
+
+#ifndef BUTTON_WIDGET
+#define BUTTON_WIDGET
+
+
+class ButtonWidget {
+
+};
+
+
+#endif //BUTTON_WIDGET
---
+++ graphics-service/src/ButtonWidget.cpp
@@ -0,0 +1,5 @@
+//
+// Created by endian on 9/12/22.
+//
+
+#include "../inc/ButtonWidget.h"
